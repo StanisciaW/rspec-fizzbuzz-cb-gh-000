e8a0aa095b66e39aad547559e874ccd03cee5d57
William Staniscia <william.staniscia@outlook.com>
2019-10-17 23:56:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,24 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-fizz_buzz =
-    (1..100).map do |i|
-        case
-        when i % 15 == 0 then "FizzBuzz"
-        when i % 3 == 0 then "Fizz"
-        when i % 5 == 0 then "Buzz"
-        else i
-        end
-    end
-puts fizz_buzz
+def fizzbuzz(number)
+  return "FizzBuzz" if is_divisible_by_fifteen?(number)
+  return "Buzz" if is_divisible_by_five?(number)
+  return "Fizz" if is_divisible_by_three?(number)
+  number
+end
+
+def is_divisible_by_three?(number)
+  is_divisible_by(number, 3)
+end
+
+def is_divisible_by_five?(number)
+  is_divisible_by(number, 5)
+end
+
+def is_divisible_by_fifteen?(number)
+  is_divisible_by(number, 15)
+end
+
+def is_divisible_by(number, divisor)
+  number % divisor == 0
+end
