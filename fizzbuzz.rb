30c09eae5e20b88dc11e37202dd85c164fb306fd
William Staniscia <william.staniscia@outlook.com>
2019-10-17 23:49:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,24 +1,53 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
-
-    expect(fizz_3).to eq("Fizz")
-  end
-  it 'returns "Buzz" when the number is divisible by 5' do
-      fizz_5 = fizzbuzz(5)
-
-      expect(fizz_5).to eq("Buzz")
-    end
-    it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-      fizz_15 = fizzbuzz(15)
-
-      expect(fizz_15).to eq("FizzBuzz")
-    end
-    it 'returns nil when the number is not divisible by 3 or 5' do
-      fizz_4 = fizzbuzz(4)
-
-      expect(fizz_4).to eq(nil)
+def fizz_buzz_1(max)
+  arr = []
+  (1..max).each do |n|
+    if ((n % 3 == 0) && (n % 5 == 0))
+      arr << "FizzBuzz"
+    elsif (n % 3 == 0)
+      arr << "Fizz"
+    elsif (n % 5 == 0)
+      arr << "Buzz"
+    else
+      arr << n
     end
   end
+  return arr
+end
+
+def fizz_buzz_2(max)
+  arr = []
+  (1..max).each do |n|
+    if (n % 3 == 0)
+      if (n % 5 == 0)
+        arr << "FizzBuzz"
+      else
+        arr << "Fizz"
+      end
+    elsif (n % 5 == 0)
+      arr << "Buzz"
+    else
+      arr << n
+    end
+  end
+  return arr
+end
+
+def fizz_buzz_3(max)
+  arr = []
+  (1..max).each do |n|
+    text = ""
+    if (n % 3 == 0)
+      text << "Fizz"
+    end
+    if (n % 5 == 0)
+      text << "Buzz"
+    end
+    if !((n % 3 == 0) || (n % 5 == 0))
+      text = n
+    end
+    arr << text
+  end
+  return arr
+end
