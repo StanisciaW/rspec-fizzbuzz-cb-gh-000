4134d6bae397d6ae0a02de462fd0db922c87b50d
William Staniscia <william.staniscia@outlook.com>
2019-10-18 00:02:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(last_number)
-  1.upto(last_number).each do |x|              
+  1.upto(last_number).each do |x|
     if (x % 3 == 0) && (x % 5 == 0)
       puts " Fizzbuzz"
     elsif x % 3 == 0
