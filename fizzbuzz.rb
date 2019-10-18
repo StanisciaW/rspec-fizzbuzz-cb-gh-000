c89037a43cdd8d0d901c75c3a7fc6ba7b2c6f4ab
William Staniscia <william.staniscia@outlook.com>
2019-10-18 00:00:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-upto(100) do |i|
-      if i % 5 == 0 and i % 3 == 0
-        puts "FizzBuzz"
-      elsif i % 5 == 0
-        puts "Buzz"
-      elsif i % 3 == 0
-        puts "Fizz"
-      else
-        puts i
-      end
+def fizzbuzz(n)
+  (1..n).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts 'fizzbuzz'
+    elsif i % 3 == 0
+      puts 'fizz'
+    elsif i % 5 == 0
+      puts 'buzz'
+    else
+      puts i
     end
+  end
+end
+
+fizzbuzz(100)
