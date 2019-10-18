ae47a1d70d075cb5038ae4a044f5953e73ba38d6
William Staniscia <william.staniscia@outlook.com>
2019-10-18 00:03:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,31 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(last_number)
-  1.upto(last_number).each do |x|
-    if (x % 3 == 0) && (x % 5 == 0)
-      puts " Fizzbuzz"
-    elsif x % 3 == 0
-      puts " Fizz"
-    elsif x % 5 == 0
-      puts " Buzz"
-    else
-      puts x
+class FizzBuzz
+  def perform
+    iterate_to(100) do |num,out|
+      out += "Fizz" if num.divisable_by?(3)
+      out += "Buzz" if num.divisable_by?(5)
+      out || num
+    end
+  end
+
+  def iterate_to(max)
+    (1..max).each do |num|
+      puts yield num,nil
     end
   end
 end
+
+class Fixnum
+  def divisable_by?(num)
+    self % num == 0
+  end
+end
+
+class NilClass
+  def +(other)
+    other
+  end
+end
+
+FizzBuzz.new.perform
